b76dc9389e9e6b203079ca0956d276dc4a5f47cc
neumanf <fabricionewman@protonmail.com>
2023-01-04 10:44:44 -0300

    feat: add package.json scripts configuration

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { bot } from "./core/bot";
 function bootstrap() {
   bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
-  bot.start();
+  bot.start({ onStart: () => console.log("[BOT] Started successfully.") });
 }
 
 bootstrap();
