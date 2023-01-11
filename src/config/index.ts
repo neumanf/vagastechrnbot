d62fb8920ec8898608e8c1eff527bebca5470af6
neumanf <fabricionewman@protonmail.com>
2023-01-11 10:50:55 -0300

    chore: remove cron environment variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,5 @@ export const config = {
   port: process.env.PORT ?? 3000,
   botToken: process.env.BOT_TOKEN ?? "",
   channelId: process.env.CHANNEL_ID ?? "",
-  routinesCron: process.env.ROUTINES_CRON ?? "*/30 * * * *",
   dbName: process.env.CYCLIC_DB ?? "",
 };
