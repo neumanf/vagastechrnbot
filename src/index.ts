24ec914cc6cf5816acf69f8a778f25de267acdd1
neumanf <fabricionewman@protonmail.com>
2024-12-19 21:29:01 -0300

    chore: store logs in a new directory

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { bot } from './core/bot';
 import { startComposer } from './commands/start';
 import { logger } from './logger/logger';
 import { setupRoutines } from './routines';
-import { RemotarJobFetcher } from './services/job-fetchers/remotar-job-fetcher';
 
 async function bootstrap() {
     bot.use(startComposer);
--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -20,6 +20,6 @@ export const logger = winston.createLogger({
 });
 
 if (process.env.NODE_ENV === 'production') {
-  logger.add(new winston.transports.File({ filename: 'error.log', level: 'error' }));
-  logger.add(new winston.transports.File({ filename: 'combined.log' }));
+  logger.add(new winston.transports.File({ filename: 'logs/error.log', level: 'error' }));
+  logger.add(new winston.transports.File({ filename: 'logs/combined.log' }));
 }
